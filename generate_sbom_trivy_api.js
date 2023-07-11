d0a98298b454f737911ca1b173e8f4537720cf0a
Hari Krishna Meda <39116208+hari61042@users.noreply.github.com>
2023-07-11 23:49:59 +0530

    Update generate_sbom_trivy_api.js

--- generate_sbom_trivy_api.js
+++ generate_sbom_trivy_api.js
@@ -31,22 +31,22 @@ app.get('/generate-sbom/:image', async (req, res) => {
     // const image = await docker.getImage(imageName).inspect();
 
     // Run 'docker-slim' to generate a detailed report
-    const command = `trivy image --format cyclonedx --output result.json ${imageName}`;
+    const command = `trivy image --format cyclonedx ${imageName}`;
     const { stdout } = await exec(command);
 
     // Save the SBOM report with the image name as the filename
-    const reportFilename = `${imageName}_sbom.json`;
-    fs.writeFileSync(reportFilename, stdout);
+    // const reportFilename = `${imageName}_sbom.json`;
+    // fs.writeFileSync(reportFilename, stdout);
 
-    console.log(`SBOM report saved as ${reportFilename}`);
+    // console.log(`SBOM report saved as ${reportFilename}`);
 
-    res.download(reportFilename, () => {
-      fs.unlinkSync(reportFilename);
-    });
+    // res.download(reportFilename, () => {
+    //   fs.unlinkSync(reportFilename);
+    // });
 
-    // const sbomReport = JSON.stringify(stdout);
+    const sbomReport = JSON.stringify(stdout);
 
-    // res.json({ sbomReport });
+    res.json({ sbomReport });
   } catch (error) {
     console.error('Error generating SBOM:', error);
     res.status(500).json({ error: 'Error generating SBOM' });
